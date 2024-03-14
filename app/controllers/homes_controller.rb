73f25d9f8cc79a37ea4481e7d636a8f96f4f29e8
usmanakram1180 <usmanakram1180@outlook.com>
2024-03-14 15:35:11 +0500

    first commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,29 @@
+class HomesController < ApplicationController
+    def new
+        @home = Home.new
+    end
+      
+    def index
+      @home = Home.new
+      @homes = Home.all
+      puts "@homes: #{@homes.inspect}"
+      @homes = Home.order(created_at: :desc)
+    end
+    def show
+      @home = Home.find(params[:id])
+    end
+    def create
+      @home = Home.new(home_params)
+      if @home.save
+        redirect_to root_path, notice: 'Post was successfully created.'
+      else
+         render :new
+      end
+    end
+    
+      private
+      def home_params
+        params.require(:home).permit(:content, :music, :video, :photo, image: [], images: [])
+      end
+    
+end
---
+++ app/models/home.rb
@@ -0,0 +1,7 @@
+class Home < ApplicationRecord
+    has_many_attached :music
+    has_one_attached :image
+    has_many_attached :images
+  has_one_attached :video
+  has_one_attached :photo
+end
